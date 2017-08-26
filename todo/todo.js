467ca338ea9db4009eac98941fcf7003d65b4e44
jhulsebos <juhubos@gmail.com>
2017-08-26 16:31:30 -0700

    add todo with enter

--- todo/todo.js
+++ todo/todo.js
@@ -2,14 +2,23 @@
 var button = document.querySelector('.addBtn')
 var entry = document.querySelector('.entry')
 
+
 button.addEventListener('click', function () {
   var entry = document.querySelector('.entry')
   text = entry.value
-  if (!text || text == '') {
+  if (!text || text == '') {          
     return false
   }
   addNewItem(document.querySelector('.todoList'), text)
 })
+
+document.querySelector("#entry").addEventListener('keypress', function(event) {
+	var key = event.which || event.keyCode;
+    if (key === 13) { 
+		var todo = this.value;
+		addNewItem(document.querySelector('.todoList'), todo);
+	 }
+});
 
 function addNewItem (list, text) {
   console.log('this is working')
@@ -21,3 +30,10 @@ function addNewItem (list, text) {
 
  // this is what output should look like
  // <li> <input type="checkbox"> new item here </li>
+
+
+
+
+
+
+
